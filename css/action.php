a54e51c0ea815264acfc05ca9a8ca0b45f94d664
Tomoaki mbp13r-2015 <tomoaki.nagahara@gmail.com>
2020-08-14 14:58:28 +0900

    New: css > action.php, body.css, color.css

---
+++ css/action.php
@@ -0,0 +1,15 @@
+<?php
+/** op-layout-huaxia:/css/action.php
+ *
+ * @created   2020-08-14
+ * @version   1.0
+ * @package   op-layout-huaxia
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+//	...
+return [
+	__DIR__.'/body',
+	__DIR__.'/color',
+];
