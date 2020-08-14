63ddfd12850b6123b9d6402b59ef0358e831f448
Tomoaki mbp13r-2015 <tomoaki.nagahara@gmail.com>
2020-08-14 14:44:50 +0900

    New: index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/** op-layout-huaxia:/index.php
+ *
+ * @created   2020-08-14
+ * @version   1.0
+ * @package   op-layout-huaxia
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+//	...
+include('html.phtml');
